78084070cdfce7f6b5044badaf2ffd122d15bcc9
jdlewis474 <jdlewis0474@gmail.com>
2024-10-01 09:49:04 -0400

    added charcount

--- main.py
+++ main.py
@@ -11,11 +11,28 @@ def word_count(text):
     word_list = text.split()
     return len(word_list)
 
+def char_split(text):
+    nested_chars = []
+    nested_chars.append(list(text))
+    chars = []
+    for word in nested_chars:
+        for char in word:
+            chars.append(char)
+    return chars
+
+def char_count(chars):
+    import string
+    alphabet = list(string.ascii_lowercase)
+    count_dict = dict.fromkeys(alphabet, 0)
+    for char in chars:
+        for letter in count_dict:
+            if char == letter:
+                count_dict[letter] += 1
+    return count_dict
 
 def main():
     booktext = get_book()
-    print(word_count(booktext))
-    
-
+    char_list = char_split(booktext)
+    print(char_count(char_list))
 
 main()
